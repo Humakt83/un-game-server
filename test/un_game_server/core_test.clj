70cba60406c7a633d96a5bfc15a58b0217b188e3
Humakt83 <urlact@yahoo.com>
2021-02-18 21:26:02 +0200

    Initial clojure files

---
+++ test/un_game_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns un-game-server.core-test
+  (:require [clojure.test :refer :all]
+            [un-game-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
