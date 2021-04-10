cf1720c6c33d9b4e7c68bb9f1ecee11c9d3c3f98
Otto Uusihakala <otto.uusihakala@gofore.com>
2021-04-10 10:56:06 +0300

    initial players state stored in atom, message handling, JSON message parsing, old functionality retained (text reversal still works)

--- src/un_game_server/core.clj
+++ src/un_game_server/core.clj
@@ -1,13 +1,25 @@
 (ns un-game-server.core
   (:require
-    [immutant.web             :as web]
-    [immutant.web.async       :as async]
-    [immutant.web.middleware  :as web-middleware]
-    [compojure.route          :as route]
-    [environ.core             :refer (env)]
-    [compojure.core           :refer (ANY GET defroutes)]
-    [ring.util.response       :refer (response redirect content-type)])
+   [immutant.web             :as web]
+   [immutant.web.async       :as async]
+   [immutant.web.middleware  :as web-middleware]
+   [compojure.route          :as route]
+   [environ.core             :refer (env)]
+   [compojure.core           :refer (ANY GET defroutes)]
+   [ring.util.response       :refer (response redirect content-type)]
+   [clojure.data.json        :as json])
   (:gen-class))
+
+(def players (atom {:player1 {:name "Player 1" :game-id "1234"}}))
+
+(defn get-players [] @players)
+
+(defn handle-message [{message-type :msgType
+                       content :content}]
+  (println "Attempting to handle message of type: " message-type " and content: " content)
+  (case message-type
+      "getplayers" (apply str (get-players))
+      "text" (apply str (reverse content))))
 
 (def websocket-callbacks
   "WebSocket callback functions"
@@ -16,7 +28,8 @@
   :on-close   (fn [channel {:keys [code reason]}]
     (println "close code:" code "reason:" reason))
   :on-message (fn [ch m]
-    (async/send! ch (apply str (reverse m))))})
+    (println "Received message " m)
+    (async/send! ch (handle-message (json/read-str m :key-fn keyword))))})
 
 (defroutes routes
   (GET "/" {c :context} (redirect (str c "/index.html")))
@@ -31,5 +44,3 @@
       (web-middleware/wrap-websocket websocket-callbacks))
       (merge {"host" (env :demo-web-host), "port" (env :demo-web-port)}
       args)))
-
-      
